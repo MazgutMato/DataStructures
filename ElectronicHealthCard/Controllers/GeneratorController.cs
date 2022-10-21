32b2de1c365484245c4bce87c4ec929caf4428bf
Matej Mažgút <mazgut13@stud.uniza.sk>
2022-10-21 09:57:16 +0200

    Generator added

---
+++ ElectronicHealthCard/Controllers/GeneratorController.cs
@@ -0,0 +1,38 @@
+﻿using DataStructures.Tree.BSTree;
+using ElectronicHealthCard.Models;
+using System.Text;
+using System;
+
+namespace ElectronicHealthCard.Controllers
+{
+    public class GeneratorController
+    {
+        private readonly Random random = new Random();
+        private string RandomString(int size)
+        {
+            var builder = new StringBuilder(size);
+
+            char offset = 'a';
+            const int lettersOffset = 26;
+
+            for (var i = 0; i < size; i++)
+            {
+                var randomChar = (char)random.Next(offset, offset + lettersOffset);
+                builder.Append(randomChar);
+            }
+
+            return builder.ToString();
+        }
+        public BSTree<Hospital> GenerateHospitals(int number)
+        {
+            var hospitals = new BSTree<Hospital>();
+            for (var i = 0; i < number; i++)
+            {
+                var nameSize = random.Next(3, 20);
+                var hospital = new Hospital(RandomString(nameSize));
+                hospitals.Add(hospital);
+            }
+            return hospitals;
+        }
+    }
+}
---
+++ ElectronicHealthCard/Models/Generator.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ElectronicHealthCard.Models
+{
+    public class Generator
+    {
+        [Required]
+        [Range(1,10000)]
+        public int HospitalNumber;
+        [Required]
+        [Range(1, 50000)]
+        public int PatientNumber;
+        [Required]
+        [Range(1, 50000)]
+        public int RecordNumber;
+
+        public Generator(int hospitalNumber, int patientNumber, int recordNumber)
+        {
+            HospitalNumber = hospitalNumber;
+            PatientNumber = patientNumber;
+            RecordNumber = recordNumber;
+        }
+    }
+}
